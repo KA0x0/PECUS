5a0748d04f0102b42d78929ec365ac73126d36d2
KA0x0 <jasonmercier666@gmail.com>
2026-05-29 18:40:35 -0400

    Speed up autoshell.sh.

--- gnu/home/services/config/bash.d/autoshell.sh
+++ gnu/home/services/config/bash.d/autoshell.sh
@@ -2,9 +2,17 @@
 set -euo pipefail
 
 # Run the given command via 'guix shell'
-function ,
-{
-    pkg_ver="$(guix locate "$1"|grep /bin/|head -1|cut -f1)"
-    pkg="$(echo $pkg_ver|cut -d@ -f1)"
-    test -n "$pkg" && guix shell "$pkg"--"$@"
+function ,() {
+    local pkg
+
+    pkg=$(
+        guix locate "$1" |
+        awk '/\/bin\// {
+            sub(/@.*/, "", $1)
+            print $1
+            exit
+        }'
+    )
+
+    [ -n "$pkg" ] && guix shell "$pkg" -- "$@"
 }

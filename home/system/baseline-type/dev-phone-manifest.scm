e69a485d276ac41da3ab92314399faac20125395
KA0x0 <jasonmercier666@gmail.com>
2026-05-30 00:45:49 -0400

    Bump.

--- home/system/baseline-type/dev-phone-manifest.scm
+++ home/system/baseline-type/dev-phone-manifest.scm
@@ -15,6 +15,7 @@
             clojure emacs-clojure-ts-mode
             diffoscope
             difftastic emacs-difftastic
+            duckdb
             pecus-emacs-next
             pecus-emacs-jwt
             guile-emacs
@@ -70,7 +71,6 @@
             go emacs-go-mode
             gopls
             delve ;; go debugger
-            duckdb
             graphviz emacs-graphviz-dot-mode
             guile-aws
             inkscape

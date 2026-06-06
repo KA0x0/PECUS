7736a51e6409f94742f7813fd053f4fb03228eb9
KA0x0 <jasonmercier666@gmail.com>
2026-06-05 23:31:15 -0400

    Remove emacs-jq-mode ;; use emacs-jq-ts-mode.

--- home/system/baseline-type/dev-phone-manifest.scm
+++ home/system/baseline-type/dev-phone-manifest.scm
@@ -37,7 +37,6 @@
             emacs-inheritenv
             emacs-jarchive
             emacs-jinx
-            emacs-jq-mode
             emacs-ligature
             emacs-lpy
             emacs-magit

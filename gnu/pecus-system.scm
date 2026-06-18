7985a15e4279483bd6b28cbfd467514a4e0538c9
KA0x0 <jasonmercier666@gmail.com>
2026-06-17 21:46:54 -0400

    Remove consult-dir from %pecus-base-packages.

--- gnu/pecus-system.scm
+++ gnu/pecus-system.scm
@@ -74,7 +74,7 @@
          emacs-cape
          emacs-casual
          emacs-combobulate
-         emacs-consult emacs-consult-dir emacs-consult-eglot
+         emacs-consult emacs-consult-eglot
          emacs-corfu
          emacs-doom-modeline
          emacs-embark emacs-embark-consult
